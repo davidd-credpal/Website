53682abba9c730c23e314155553867e407fe1851
David Dennis <david.d@credpal.com>
2020-08-11 04:38:03 +0100

    Setup app configurations

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!--  -->
+    <PersonalHeaderComponent />
+  </div>
+</template>
+
+<script>
+import PersonalHeaderComponent from "@/components/PersonalHeaderComponent.vue";
+// import BusinessHeaderComponent from "@/components/BusinessHeaderComponent.vue";
+export default {
+  name: "app",
+  components: {
+    PersonalHeaderComponent
+    // BusinessHeaderComponent
+  }
+};
+</script>
+
+<style lang="scss">
+@import "assets/css/app";
+#app {
+  font-family: AxiformaBook, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/PersonalHeaderComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div id="nav">
+      <div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PersonalHeaderComponent"
+};
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
